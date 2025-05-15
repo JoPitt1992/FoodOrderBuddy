24edcd3725e52438dba207d6e855aa5b92ded536
benjaminmader <127972854+benjaminmader@users.noreply.github.com>
2025-05-15 20:29:48 +0200

    Update data.sql. An Menu.java angeglichen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO menu (VERSION, menu_name, menu_ingredients, menu_price) VALUES
-(1, 'Pizza Margherita', 'Tomate, Mozzarella', 10.90),
-(1, 'Pizza Diavolo', 'Tomate, Mozzarella, Salami (scharf), Pepperoni', 12.90),
-(1, 'Lasagne al forno', 'Nudeln, Bolognese, Mozzarella', 14.90),
-(1, 'Grillteller des Hauses', 'Steaks vom Schwein, Rind und Pute, dazu Pommes Frites', 21.90);
+INSERT INTO menu (menu_title, menu_ingredients, menu_price, menu_daily) VALUES
+('Pizza Margherita', 'Tomate, Mozzarella', 10.90, false),
+('Pizza Diavolo', 'Tomate, Mozzarella, Salami (scharf), Pepperoni', 12.90, true),
+('Lasagne al forno', 'Nudeln, Bolognese, Mozzarella', 14.90, false),
+('Grillteller des Hauses', 'Steaks vom Schwein, Rind und Pute, dazu Pommes Frites', 21.90, false);
