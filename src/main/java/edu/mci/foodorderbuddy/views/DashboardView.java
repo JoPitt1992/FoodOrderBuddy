7ef84e444625b8896ab4d3f188242915c57d3e4a
benjaminmader <127972854+benjaminmader@users.noreply.github.com>
2025-05-18 12:55:37 +0200

    Database Test mit öffentlicher ip adresse

--- src/main/java/edu/mci/foodorderbuddy/views/DashboardView.java
+++ src/main/java/edu/mci/foodorderbuddy/views/DashboardView.java
@@ -22,7 +22,7 @@ public class DashboardView extends VerticalLayout {
     }
 
     private Component exampleMethod() {
-        Span message = new Span("DashboardView-Site under construction");
+        Span message = new Span("DashboardView-Site under construction - by benni");
         message.addClassNames("text-xl", "mt-m");
         return message;
     }
